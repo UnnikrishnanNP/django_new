05fdd91440d5caff00619eb22869d436067b29a0
UnnikrishnanNP12 <uknp0371@gmail.com>
2021-07-31 20:19:25 +0530

    done till dynamic urls

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
-def home_page(request):
-    return HttpResponse('Hello World')
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        'leads' : leads,
+    }
+    return render(request,'lead_list.html',context)
+
+def lead_details(request,pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead' : lead
+    }
+    return render(request,'lead_details.html',context)
